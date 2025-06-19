a1d5d3e018b1b3e93aa8ae0c18266bccbbf48ef6
ind-nt <52841347+ind-nt@users.noreply.github.com>
2025-06-19 12:18:57 -0600

    path correction

--- js/load-sw.js
+++ js/load-sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
@@ -27,7 +27,7 @@ function hideLoadingOverlay() {
 // Service Worker Registration (your existing code, slightly modified)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
 
